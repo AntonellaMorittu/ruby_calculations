023f634e625088dc4fcb18684f63b1e2682207f8
Antonella_Morittu <morittuantonella@gmail.com>
2017-12-12 20:07:32 +0100

    more exercises

---
+++ strings.rb
@@ -0,0 +1,48 @@
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+puts ''
+puts 'Merry Christmas..'
+puts ''
+puts '..And Happy 2018!'
+puts ''
+puts 'I like ' + 'apple pie'
+puts 'I like' + ' apple pie'
+puts 'blink ' * 4
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+puts 'You\'re swell!'
+puts 'backlash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'
+var1 = 2
+var2 = '5'
+
+puts var1.to_s + var2
+
+var1 = 2
+var2 = '5'
+
+puts var1.to_s + var2
+puts var1 + var2.to_i
+
+puts '15'.to_f
+puts '99.999'.to_f
+puts '99.999'.to_i
+puts ''
+puts '5 is  my favourite number!'.to_i
+puts 'Who asked you about 5 or whatever?'.to_i
+puts 'Your momma did.'.to_f
+puts ''
+puts 'stringy'.to_s
+puts 3.to_i
+
+
+puts 'Hello there, and what\'s your name?'
+name = gets.chomp
+puts 'Your name is ' + name + '? What a a lovely name!'
+puts 'Pleased to meet you, ' + name + '. :)'
