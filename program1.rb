0b119f4abd04e9d02a28b376cec92c5a4c0f9ba4
Antonella_Morittu <morittuantonella@gmail.com>
2017-12-12 20:19:36 +0100

    My first program

---
+++ program1.rb
@@ -0,0 +1,7 @@
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+puts 'And your middle name?'
+middle = gets.chomp
+puts 'What is your last name?'
+last = gets.chomp
+puts 'Hello ' + name + middle + last + '!'
